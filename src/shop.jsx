5f3cd1afebfc3b1ec87b8aa4f2434cb1d718266c
Rafael Pacheco <rafaelpache64@outlook.com>
2023-05-27 15:42:21 -0400

    I styled the SectionTwo of shop.html page

--- src/shop.jsx
+++ src/shop.jsx
@@ -5,15 +5,23 @@ import ReactDOM from 'react-dom/client';
 // Components and sass
 import './styles.scss';
 import Header from './components/header';
-import SectionOne from './components/section1';
 import SectionTwo from './components/section2';
-import BrandSection from './components/brandSection';
+import ShopSection from './components/shopSection';
 import SectionThree from './components/section3';
 import Footer from './components/footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <SectionTwo
+      textTitleSectionTwo="SHOPING WITHOUT LIMITS"
+      textParaSectionTwo="You can choose the best option for you, and it does not matter whether you are in Prague or San Fransisco"
+      linkText="SHOW NOW"
+      classesSection2="section2-shop1"
+    />
+
+    <ShopSection />
+
     <Footer />
   </React.StrictMode>
 );
