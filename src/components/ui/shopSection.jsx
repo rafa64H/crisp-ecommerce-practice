ef3163c3de4f57c12a2e4968cf4e0bf890f04e29
Rafael Pacheco <rafaelpache64@outlook.com>
2023-05-29 16:53:36 -0400

    I started to make the template for each clothesCard.jsx for the entire project.

--- src/components/ui/shopSection.jsx
+++ src/components/ui/shopSection.jsx
@@ -3,10 +3,22 @@ import ClothesCard from './smaller/clothesCard';
 
 import clothesData from '../../data/clothes_data.json';
 
-const ShopSection = () => (
-  <section className="shop-section">
-    <ul className="shop-section-list" />
-  </section>
-);
+const ShopSection = () => {
+  console.log(clothesData[0]);
+  return (
+    <section className="shop-section">
+      <ul className="shop-section-list">
+        <ClothesCard
+          productName={clothesData[0][0].productName}
+          productColors={clothesData[0][0].colors}
+          productImg={clothesData[0][0].otherImages[0]}
+          gender={clothesData[0][0].gender}
+          price={clothesData[0][0].price}
+          category={clothesData[0][0].category}
+        />
+      </ul>
+    </section>
+  );
+};
 
 export default ShopSection;
--- src/components/ui/smaller/clothesCard.jsx
+++ src/components/ui/smaller/clothesCard.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-const ClothesCard = () => (
-  <li>
-    <img src="" alt="" />
-    <p />
-    <h3 />
-    <p />
-    <div />
-  </li>
-);
+const ClothesCard = ({
+  productImg,
+  category,
+  gender,
+  productName,
+  productPrice,
+  productColors,
+}) => {
+  function handleDefault(e) {
+    e.preventDefault();
+  }
+  const productColorsButtons = productColors.map((color) => (
+    <button
+      type="button"
+      className={`clothes-card-colors__button ${color.class}`}
+      aria-label={`${color.name}`}
+      key={uuidv4()}
+    />
+  ));
+
+  return (
+    <li className="clothes-card-li">
+      <a href="#" className="clothes-card">
+        <img className="clothes-card__img" src={productImg} alt={productName} />
+        <p className="clothes-card__category-gender">
+          {category} {gender}
+        </p>
+        <h3 className="clothes-card__name">{productName}</h3>
+        <p className="clothes-card__price">
+          <strong>{productPrice}</strong>
+        </p>
+
+        <div className="clothes-card-colors" onClick={(e) => handleDefault(e)}>
+          {productColorsButtons}
+        </div>
+      </a>
+    </li>
+  );
+};
 
 export default ClothesCard;
