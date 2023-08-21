764d6de4cd2e7015544ea815966e9d3ca7559085
Rafael Pacheco <rafaelpache64@outlook.com>
2023-08-21 14:36:18 -0400

    I wrote the list items of community into a component, also added files to code createPost page.

---
+++ src/pages/create-post/createPost.jsx
@@ -0,0 +1,22 @@
+// Index page
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// data
+import clothesData from '../../data/clothes_data.json';
+
+// Components and sass
+import '../../assets/styles.scss';
+import Header from '../../components/ui/header';
+import Footer from '../../components/ui/footer';
+import SectionThree from '../../components/ui/section3';
+import CreateAccountForm from './createAccountForm';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header />
+
+    <SectionThree />
+    <Footer />
+  </React.StrictMode>
+);
