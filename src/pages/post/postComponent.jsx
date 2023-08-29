3906f0ad6ed60d33d9137d9c77ab027037e21792
Rafael Pacheco <rafaelpache64@outlook.com>
2023-08-29 12:16:34 -0400

    Now you can reply to comments and it gets added on firestore.

--- src/pages/post/postComponent.jsx
+++ src/pages/post/postComponent.jsx
@@ -141,6 +141,8 @@ const PostComponent = () => {
                   commentLikes={comment.commentLikes}
                   commentDislikes={comment.commentDislikes}
                   commentReplies={comment.commentReplies}
+                  post={post}
+                  setPost={setPost}
                 />
               ))}
             </ul>
@@ -167,81 +169,186 @@ const CommentItem = ({
   commentLikes,
   commentDislikes,
   commentReplies,
-}) => (
-  <li className="comment">
-    <div className="comment-user">
-      <i className="fa-solid fa-user comment-user__icon" />
-      <p className="comment-user__paragraph">
-        {commentUser} {commentDay}/0{commentMonth}/{commentYear}
-      </p>
-    </div>
-    <p className="comment-text">{commentText}</p>
-    <div className="like-dislike-container">
-      <button
-        onClick={(e) => e.preventDefault()}
-        type="button"
-        alt="Like"
-        className="like-dislike like"
+  post,
+  setPost,
+}) => {
+  const [showWriteReply, setShowWriteReply] = useState(false);
+
+  const [commentRepliesState, setCommentRepliesState] =
+    useState(commentReplies);
+
+  const [showReplies, setShowReplies] = useState([]);
+
+  const replyRef = useRef('');
+
+  async function handleSubmitReply(e) {
+    e.preventDefault();
+
+    if (!replyRef.current.value) return null;
+
+    const today = new Date();
+
+    const todayDate = today.getDate();
+    const todayMonth = today.getMonth() + 1;
+    const todayYear = today.getFullYear();
+
+    try {
+      const { postComments } = post;
+
+      const userData = await getDataOfUser();
+
+      const newReply = {
+        replyId: uuidv4(),
+        replyUser: `${userData.firstName} ${userData.lastName}`,
+        replyUserUid: userData.uid,
+        replyDay: todayDate,
+        replyMonth: todayMonth,
+        replyYear: todayYear,
+        replyText: replyRef.current.value,
+        replyLikes: [],
+        replyDislikes: [],
+      };
+
+      const theComment = postComments.find(
+        (comment) => commentId === comment.commentId
+      );
+      const indexOfComment = postComments.indexOf(theComment);
+
+      theComment.commentReplies = [...theComment.commentReplies, newReply];
+
+      const newPostInfo = post;
+
+      setCommentRepliesState(
+        newPostInfo.postComments[indexOfComment].commentReplies
+      );
+
+      await updateSpecifiedPost(newPostInfo);
+
+      setPost(newPostInfo);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleShowReplies() {
+    const numberOfItemsToShow = [...showReplies];
+
+    for (let i = showReplies.length; i <= showReplies.length + 4; i++) {
+      if (i >= commentRepliesState.length) break;
+      numberOfItemsToShow.push(i);
+    }
+
+    console.log(numberOfItemsToShow);
+
+    setShowReplies((prevValue) => [...numberOfItemsToShow]);
+  }
+
+  return (
+    <li className="comment">
+      <div className="comment-user">
+        <i className="fa-solid fa-user comment-user__icon" />
+        <p className="comment-user__paragraph">
+          {commentUser} {commentDay}/0{commentMonth}/{commentYear}
+        </p>
+      </div>
+      <p className="comment-text">{commentText}</p>
+      <div className="like-dislike-container">
+        <button
+          onClick={(e) => e.preventDefault()}
+          type="button"
+          alt="Like"
+          className="like-dislike like"
+        >
+          <i className="fa-solid fa-arrow-up" />
+          <p>{commentLikes}</p>
+        </button>
+
+        <button
+          onClick={(e) => e.preventDefault()}
+          type="button"
+          alt="Dislike"
+          className="like-dislike dislike"
+        >
+          <i className="fa-solid fa-arrow-down" />
+          <p>{commentDislikes}</p>
+        </button>
+      </div>
+
+      <div className="reply-buttons">
+        <button
+          type="button"
+          className="transparent-btn reply-btn"
+          onClick={(e) => setShowWriteReply((prevValue) => !prevValue)}
+        >
+          Reply <i className="fa-solid fa-reply" />
+        </button>
+
+        <button
+          type="button"
+          className="transparent-btn comment-show-replies"
+          onClick={(e) => handleShowReplies(e)}
+        >
+          Show replies <i className="fa-solid fa-plus" />
+        </button>
+      </div>
+
+      <form
+        className="write-reply"
+        data-show-write-reply={showWriteReply}
+        onSubmit={(e) => {
+          handleSubmitReply(e);
+        }}
       >
-        <i className="fa-solid fa-arrow-up" />
-        <p>{commentLikes}</p>
-      </button>
+        <textarea className="write-reply__textarea" ref={replyRef} />
+        <button type="submit" className="black-btn write-reply__btn">
+          Submit
+        </button>
+      </form>
 
-      <button
-        onClick={(e) => e.preventDefault()}
-        type="button"
-        alt="Dislike"
-        className="like-dislike dislike"
-      >
-        <i className="fa-solid fa-arrow-down" />
-        <p>{commentDislikes}</p>
-      </button>
-    </div>
+      <ul className="comment-replies">
+        {commentRepliesState.map((reply, index) => (
+          <li
+            className="comment-reply"
+            data-show-reply={showReplies.some(
+              (indexState) => index === indexState
+            )}
+            key={uuidv4()}
+          >
+            <div className="comment-user">
+              <i className="fa-solid fa-user comment-user__icon" />
+              <p className="comment-user__paragraph">
+                {reply.replyUser} {reply.replyDay}/0{reply.replyMonth}/
+                {reply.replyYear}
+              </p>
+            </div>
+            <p className="comment-text">{reply.replyText}</p>
+            <div className="like-dislike-container">
+              <button
+                onClick={(e) => e.preventDefault()}
+                type="button"
+                alt="Like"
+                className="like-dislike like"
+              >
+                <i className="fa-solid fa-arrow-up" />
+                <p>{reply.replyLikes.length}</p>
+              </button>
 
-    <div className="reply-buttons">
-      <button type="button" className="transparent-btn reply-btn">
-        Reply <i className="fa-solid fa-reply" />
-      </button>
-
-      <button type="button" className="transparent-btn comment-show-replies">
-        Show replies <i className="fa-solid fa-plus" />
-      </button>
-    </div>
-
-    <ul className="comment-replies">
-      {commentReplies.map((reply) => (
-        <li className="comment-reply" key={uuidv4()}>
-          <div className="comment-user">
-            <i className="fa-solid fa-user comment-user__icon" />
-            <p className="comment-user__paragraph">
-              {reply.replyUser} {reply.replyDay}/0{reply.replyMonth}/
-              {reply.replyYear}
-            </p>
-          </div>
-          <p className="comment-text">{reply.replyText}</p>
-          <div className="like-dislike-container">
-            <button
-              onClick={(e) => e.preventDefault()}
-              type="button"
-              alt="Like"
-              className="like-dislike like"
-            >
-              <i className="fa-solid fa-arrow-up" />
-              <p>{reply.replyLikes.length}</p>
-            </button>
-
-            <button
-              onClick={(e) => e.preventDefault()}
-              type="button"
-              alt="Dislike"
-              className="like-dislike dislike"
-            >
-              <i className="fa-solid fa-arrow-down" />
-              <p>{reply.replyDislikes.length}</p>
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </li>
-);
+              <button
+                onClick={(e) => e.preventDefault()}
+                type="button"
+                alt="Dislike"
+                className="like-dislike dislike"
+              >
+                <i className="fa-solid fa-arrow-down" />
+                <p>{reply.replyDislikes.length}</p>
+              </button>
+            </div>
+          </li>
+        ))}
+        <div>
+          There are {commentRepliesState.length - showReplies.length} left
+        </div>
+      </ul>
+    </li>
+  );
+};
