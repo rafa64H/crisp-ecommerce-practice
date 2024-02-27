384b569bf5f9ec50ef7761989fe996c15b9dd649
Rafael Pacheco <rafaelpache64@outlook.com>
2024-02-27 15:03:53 -0400

    small quotes in firebase-template.js

--- src/config-firebase/firebase-template.js
+++ src/config-firebase/firebase-template.js
@@ -1,21 +1,23 @@
-// The filename should be firebase.js and should be like this
+// The filename should be named "firebase.js" and should be like this
 
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: 'Placeholder',
-  authDomain: 'Placeholder',
-  projectId: 'Placeholder',
-  storageBucket: 'Placeholder',
-  messagingSenderId: 'Placeholder',
-  appId: 'Placeholder',
+  apiKey: "Placeholder",
+  authDomain: "Placeholder",
+  projectId: "Placeholder",
+  storageBucket: "Placeholder",
+  messagingSenderId: "Placeholder",
+  appId: "Placeholder",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
